bbf50b7510b8e02354b1302e8a4052c9a2a6cd4a
Ashlesha Barekar <barekarashlesha04@gmail.com>
2025-01-08 18:57:27 +0530

    For loop

---
+++ Add.java
@@ -0,0 +1,12 @@
+public class Add {
+    public static void main(String[] args) {
+        int num1 = 10;
+
+        for (int i = 1; i <= num1; i++) {
+            System.out.println(i);
+        }
+        ;
+
+    }
+
+}
